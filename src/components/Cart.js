a44cfd87012a97b4d0c5c15c4efbad119d547102
Mohamed Walid <mohamed.walid1999@gmail.com>
2021-09-22 15:25:34 +0200

    feat: update cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getAllProducts } from "../services/product.service";
+import { getAllProducts, getProductDetails } from "../services/product.service";
 import CartBody from "./CartBody";
+import { cartStorage } from "../storage/cartStorage";
 
-export  function Cart(props) {
+export function Cart(props) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const cart = cartStorage.getCart();
+      console.log(cart);
+      for (let i = 0; i < cart.length; i++) {
+        try {
+          const response = await getProductDetails(cart[i].id);
+          const product = response.data.product;
+          product.quantity = cart[i].quantity;
+          product.price = product.price * product.quantity;
+          console.log(product);
+          setProducts([...products,product]);
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const response = await getAllProducts()
-            setProducts(response.data.products);
-            console.log(response.data.products)
-        })()
-    }, [])
+      console.log(products);
+    })();
+  }, [products]);
   return (
     <div>
       <table>
@@ -25,17 +38,17 @@ export  function Cart(props) {
           </tr>
         </thead>
         <tbody>
-        {products.map((product, index) => (
-                        <CartBody 
-                        key={product._id}
-                        id={product._id}
-                        img={product.img}
-                        name={product.title}
-                        price={product.price}/>
-            ))}
+          {products.map((product, index) => (
+            <CartBody
+              key={product._id}
+              id={product._id}
+              img={product.img}
+              name={product.title}
+              price={product.price}
+            />
+          ))}
         </tbody>
       </table>
     </div>
   );
 }
-

10cd4435b4b45fd8f301756777ad9a752671be95
ziad1299 <34491178+ziad1299@users.noreply.github.com>
2021-09-22 21:50:14 +0200

    final register-login

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,46 +1,80 @@
 import classes from "../styles/Forms.module.css";
 import Card from "./ui/Card";
 import Layout from "./layout/Layout";
+import { useEffect, useState} from "react";
+import { register } from "../services/user.service";
+import { useHistory } from "react-router-dom";
 
 function Register() {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function passwordHandler() {
-        var x = document.getElementById("password");
-        if (x.type === "password") {
-          x.type = "text";
-        } else {
-          x.type = "password";
-        }
-      }
+  function passwordHandler() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  async function registerHandler() {
+    try {
+      await register(name, email, password);
+      console.log("loading");
+      history.push("/");
+    } catch (error) {
+      alert(error.response.data.error.message);
+    }
+  }
+
   return (
     <Layout>
-        <h1>Create An Account</h1>
+      <h1>Create An Account</h1>
       <Card>
         <h3>Sign Up With Email Address</h3>
         <form className={classes.form}>
           <div className={classes.control}>
-            <input type="text" required id="name" />
+            <input
+              type="text"
+              required
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+            />
             <span className={classes.floatinglabel}>Name</span>
           </div>
           <div className={classes.control}>
-            <input type="text" required id="email" />
+            <input
+              type="text"
+              required
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <span className={classes.floatinglabel}>Email</span>
           </div>
           <div className={classes.control}>
-            <input type="password" required id="password"/>
+            <input
+              type="password"
+              required
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <span className={classes.floatinglabel}>Password</span>
           </div>
           <div>
             <label htmlFor="check">Show Password</label>
-            <input type="checkbox" onClick={passwordHandler}/>
-            </div>
-          <div className={classes.actions}>
-            <label htmlFor="notice">By registering you agreed to our <a href="#">Terms and Conditions.</a></label>
+            <input type="checkbox" onClick={passwordHandler} />
           </div>
           <div className={classes.actions}>
-            <button>CREATE AN ACCOUNT</button>
+            <label htmlFor="notice">
+              By registering you agreed to our{" "}
+              <a href="#">Terms and Conditions.</a>
+            </label>
           </div>
-         
+          <div className={classes.actions}>
+            <button type="button" onClick={registerHandler}>CREATE AN ACCOUNT</button>
+          </div>
         </form>
       </Card>
     </Layout>
